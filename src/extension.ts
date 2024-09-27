d35beb0f232ff953fa529a48b2d4659f3874e00c
Richard Zilahi <zilahi@gmail.com>
2024-09-27 22:04:16 +0300

    automatically activating plugin

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,37 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Plugin activated');
+    console.log('Gitodo Plugin activated');
 
     const disposable = vscode.commands.registerCommand('gitodo-test.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         vscode.window.showInformationMessage(
-            'Hello World from Awesome VSCode Extension Boilerplate!',
+            `${new Date().toLocaleTimeString()} Hello World from Gitodo!`,
         );
+    });
+
+    const todoCheck = () => {
+        const editor = vscode.window.activeTextEditor;
+
+        if (!editor) {
+            return;
+        }
+        const document = editor.document;
+        const selection = editor.selection;
+
+        // get the linke where the cursor is
+        const thisLineText = document.lineAt(selection.active.line).text;
+        const thisLineNumber = selection.active.line;
+
+        // chec if this line contains a todo
+        if (thisLineText.includes('TODO') || thisLineText.includes('todo')) {
+            const logMessage = `Line ${thisLineNumber + 1} contains a TODO`;
+            vscode.window.showInformationMessage(logMessage);
+        }
+    };
+
+    vscode.window.onDidChangeTextEditorSelection(() => {
+        todoCheck();
     });
 
     context.subscriptions.push(disposable);
